2879e35e77d3c0414e51a9fe7ae0778081ecb3d4
Yaseen <yaseen.alqasim@gmail.com>
2026-05-11 21:43:36 +0300

    build homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,57 @@
 "use client";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Dentist Clinic Works ✅</h1>
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        fontFamily: "Arial",
+        gap: "20px",
+      }}
+    >
+      <h1>Dental Clinic System</h1>
+
+      <Link href="/doctor">
+        <button
+          style={{
+            padding: "15px",
+            width: "250px",
+            fontSize: "18px",
+          }}
+        >
+          صفحة الطبيب
+        </button>
+      </Link>
+
+      <Link href="/secretary">
+        <button
+          style={{
+            padding: "15px",
+            width: "250px",
+            fontSize: "18px",
+          }}
+        >
+          صفحة السكرتيرة
+        </button>
+      </Link>
+
+      <Link href="/reports">
+        <button
+          style={{
+            padding: "15px",
+            width: "250px",
+            fontSize: "18px",
+          }}
+        >
+          التقارير
+        </button>
+      </Link>
     </main>
   );
 }
