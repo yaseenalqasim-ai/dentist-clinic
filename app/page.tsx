15bcec7156ba1f0387307cdf7019f868037e24cb
Yaseen <yaseen.alqasim@gmail.com>
2026-05-11 21:34:44 +0300

    remove localstorage

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,9 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 export default function Home() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    const saved = window.localStorage.getItem("data");
-
-    if (saved) {
-      setData(saved);
-    }
-  }, []);
-
   return (
-    <main
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-        fontFamily: "Arial",
-      }}
-    >
-      <h1>Dentist Clinic</h1>
-
-      <input
-        type="text"
-        placeholder="اكتب شيئاً"
-        value={data}
-        onChange={(e) => {
-          setData(e.target.value);
-          window.localStorage.setItem("data", e.target.value);
-        }}
-        style={{
-          padding: "10px",
-          marginTop: "20px",
-          width: "300px",
-        }}
-      />
-
-      <p style={{ marginTop: "20px" }}>
-        القيمة المحفوظة: {data}
-      </p>
+    <main>
+      <h1>Dentist Clinic Works ✅</h1>
     </main>
   );
 }
